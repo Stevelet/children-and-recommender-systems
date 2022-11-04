6c20487735372196b8a344cf8edb12abe32ca1a0
Yessin <yessinbeyhan@gmail.com>
2022-11-04 15:21:29 +0100

    Blacklist books without language code

--- merge_script.py
+++ merge_script.py
@@ -10,8 +10,8 @@ import json
 
 # Goodreads
 with open("data/goodreads.json") as f:
-    # breaker = 1
-    language_whitelist = ["", "eng", "en-GB", "en-US"]
+    breaker = 1
+    language_whitelist = ["eng", "en-GB", "en-US"]
 
     no_lang = 0
     eng = 0
@@ -19,25 +19,14 @@ with open("data/goodreads.json") as f:
     en_US = 0
 
     for row in f:
-        # breaker += 1
-        # if breaker == 100:
-        #     break
+        breaker += 1
+        if breaker == 100:
+            break
 
         j = json.loads(row)
 
         if j["isbn"] != "":
             if j["language_code"] in language_whitelist:
-                if j["language_code"] == "":
-                    no_lang += 1
-                if j["language_code"] == "eng":
-                    eng += 1
-                if j["language_code"] == "en-GB":
-                    en_GB += 1
-                if j["language_code"] == "en-US":
-                    en_US += 1
+                print(j)
 
-    print(no_lang)
-    print(eng)
-    print(en_GB)
-    print(en_US)
     f.close()
