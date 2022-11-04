aaa10b07ce6f3fa14d380be5a3e394db5e2c1efb
Yessin <yessinbeyhan@gmail.com>
2022-11-04 15:51:31 +0100

    Make first setup for adding line to csv

--- merge_script.py
+++ merge_script.py
@@ -9,24 +9,21 @@ import json
 #     f.close()
 
 # Goodreads
-with open("data/goodreads.json") as f:
-    breaker = 1
-    language_whitelist = ["eng", "en-GB", "en-US"]
+with open("data/goodreads.json", 'r') as f_j:
+    with open("data/combined_dataset.csv", 'a', newline='') as f_c:
+        writer = csv.writer(f_c)
 
-    no_lang = 0
-    eng = 0
-    en_GB = 0
-    en_US = 0
+        breaker = 0
+        language_whitelist = ["eng", "en-GB", "en-US"]
 
-    for row in f:
-        breaker += 1
-        if breaker == 100:
-            break
+        for row in f_j:
+            breaker += 1
+            if breaker == 100:
+                break
 
-        j = json.loads(row)
+            j = json.loads(row)
 
-        if j["isbn"] != "":
-            if j["language_code"] in language_whitelist:
-                print(j)
-
-    f.close()
+            if j["isbn"] != "":
+                if j["language_code"] in language_whitelist:
+                    writer.writerow([j["isbn"]])
+                    print(j)
